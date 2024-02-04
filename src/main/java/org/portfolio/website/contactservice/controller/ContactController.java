cdd8b60c33a52e4f677e9421dbe9f5c370f3a6cd
Dhruv Maheshwari <mrdm026@gmail.com>
2024-02-04 21:11:45 +0530

    added cross origin

--- src/main/java/org/portfolio/website/contactservice/controller/ContactController.java
+++ src/main/java/org/portfolio/website/contactservice/controller/ContactController.java
@@ -8,14 +8,12 @@ import org.portfolio.website.contactservice.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin
 public class ContactController {
 
     @Autowired
